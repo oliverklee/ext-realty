5404d5e613b68dcd2bfa65666ab92ebed2eb4e16
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-03-10 22:48:35 +0000

    Released oelib version 0.4.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/branches/version_0_1_x@8528 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'php' => '5.1.0-0.0.0',
 			'typo3' => '4.1.2-0.0.0',
 			'cms' => '',
-			'oelib' => '0.3.99-',
+			'oelib' => '0.4.0-',
 		),
 		'conflicts' => array(
 		),
