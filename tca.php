8f6a4325465f5af1d6af843316107f3672274322
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-09 19:22:14 +0000

    [Bug 1775] Drop-down lists in the BE realty object editor should be shorted by title, patch by Sven Braun, r=me
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/branches/version_0_1_x@8863 735d13b6-9817-0410-8766-e36946ffe9aa

--- tca.php
+++ tca.php
@@ -138,7 +138,7 @@ $TCA['tx_realty_objects'] = array(
 			'config' => array(
 				'type' => 'select',
 				'foreign_table' => 'tx_realty_cities',
-				'foreign_table_where' => 'AND tx_realty_cities.pid=###STORAGE_PID### ORDER BY tx_realty_cities.uid',
+				'foreign_table_where' => 'AND tx_realty_cities.pid=###STORAGE_PID### ORDER BY tx_realty_cities.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -176,7 +176,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_districts',
-				'foreign_table_where' => 'AND tx_realty_districts.pid=###STORAGE_PID### ORDER BY tx_realty_districts.uid',
+				'foreign_table_where' => 'AND tx_realty_districts.pid=###STORAGE_PID### ORDER BY tx_realty_districts.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -318,7 +318,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_apartment_types',
-				'foreign_table_where' => 'AND tx_realty_apartment_types.pid=###STORAGE_PID### ORDER BY tx_realty_apartment_types.uid',
+				'foreign_table_where' => 'AND tx_realty_apartment_types.pid=###STORAGE_PID### ORDER BY tx_realty_apartment_types.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -356,7 +356,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_house_types',
-				'foreign_table_where' => 'AND tx_realty_house_types.pid=###STORAGE_PID### ORDER BY tx_realty_house_types.uid',
+				'foreign_table_where' => 'AND tx_realty_house_types.pid=###STORAGE_PID### ORDER BY tx_realty_house_types.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -458,7 +458,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_heating_types',
-				'foreign_table_where' => 'AND tx_realty_heating_types.pid=###STORAGE_PID### ORDER BY tx_realty_heating_types.uid',
+				'foreign_table_where' => 'AND tx_realty_heating_types.pid=###STORAGE_PID### ORDER BY tx_realty_heating_types.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -496,7 +496,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_car_places',
-				'foreign_table_where' => 'AND tx_realty_car_places.pid=###STORAGE_PID### ORDER BY tx_realty_car_places.uid',
+				'foreign_table_where' => 'AND tx_realty_car_places.pid=###STORAGE_PID### ORDER BY tx_realty_car_places.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -552,7 +552,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_pets',
-				'foreign_table_where' => 'AND tx_realty_pets.pid=###STORAGE_PID### ORDER BY tx_realty_pets.uid',
+				'foreign_table_where' => 'AND tx_realty_pets.pid=###STORAGE_PID### ORDER BY tx_realty_pets.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
@@ -606,7 +606,7 @@ $TCA['tx_realty_objects'] = array(
 					array('', 0)
 				),
 				'foreign_table' => 'tx_realty_conditions',
-				'foreign_table_where' => 'AND tx_realty_conditions.pid=###STORAGE_PID### ORDER BY tx_realty_conditions.uid',
+				'foreign_table_where' => 'AND tx_realty_conditions.pid=###STORAGE_PID### ORDER BY tx_realty_conditions.title',
 				'size' => 1,
 				'minitems' => 0,
 				'maxitems' => 1,
