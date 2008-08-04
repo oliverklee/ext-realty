7fec012d44f8a162dbf13bf09803fc475af5697b
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-04 21:16:56 +0000

    [#1886] The config check should also check that the static template is included at all, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@10944 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_realty_configcheck.php
+++ class.tx_realty_configcheck.php
@@ -179,6 +179,7 @@ class tx_realty_configcheck extends tx_oelib_configcheck {
 		$this->checkDateFormat();
 		$this->checkWhatToDisplay();
 		$this->checkLocale();
+		$this->checkStaticIncluded();
 	}
 
 	/**
