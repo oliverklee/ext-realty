e04fa8853d0eee344a21380778f204705e1210b7
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-03 16:56:52 +0000

    [Bug 2356] Make the realty object inherit tx_oelib_Model, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@16339 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Mapper/class.tx_realty_Mapper_RealtyObject.php
@@ -0,0 +1,52 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('realty') . 'lib/tx_realty_constants.php');
+
+/**
+ * Class 'tx_realty_Mapper_RealtyObject' for the 'realty' extension.
+ *
+ * This class represents a mapper for realty objects.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_Mapper_RealtyObject extends tx_oelib_DataMapper {
+	/**
+	 * @var string the name of the database table for this mapper
+	 */
+	protected $tableName = REALTY_TABLE_OBJECTS;
+
+	/**
+	 * @var string the model class name for this mapper, must not be empty
+	 */
+	protected $modelClassName = 'tx_realty_Model_RealtyObject';
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/Mapper/class.tx_realty_Mapper_RealtyObject.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/Mapper/class.tx_realty_Mapper_RealtyObject.php']);
+}
+?>
