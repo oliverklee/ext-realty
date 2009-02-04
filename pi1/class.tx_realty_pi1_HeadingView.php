c4ad0bf7457fb6ed4b65bae259ac9cf546ca2130
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-04 17:51:38 +0000

    [Bug 2452] Further split the single view parts into separate views, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@16442 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/class.tx_realty_pi1_HeadingView.php
@@ -0,0 +1,63 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_realty_pi1_HeadingView' for the 'realty' extension.
+ *
+ * This class renders the heading of a single realty object.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_pi1_HeadingView extends tx_realty_pi1_FrontEndView {
+	/**
+	 * Returns the heading view as HTML.
+	 *
+	 * @param array piVars array, must contain the key "showUid" with a valid
+	 *              realty object UID as value
+	 *
+	 * @return string HTML for the heading view or an empty string if the
+	 *                realty object with the provided UID has no title
+	 */
+	public function render(array $piVars = array()) {
+		$this->setOrDeleteMarkerIfNotEmpty(
+			'title',
+			htmlspecialchars(tx_oelib_MapperRegistry
+				::get('tx_realty_Mapper_RealtyObject')
+				->find($piVars['showUid'])->getProperty('title')
+			),
+			'',
+			'field_wrapper'
+		);
+
+		return $this->getSubpart('FIELD_WRAPPER_TITLE');
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_HeadingView.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_HeadingView.php']);
+}
+?>
---
+++ tests/tx_realty_pi1_HeadingView_testcase.php
@@ -0,0 +1,141 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Unit tests for the tx_realty_pi1_HeadingView class in the 'realty'
+ * extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_pi1_HeadingView_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_realty_pi1_HeadingView
+	 */
+	private $fixture;
+
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	/**
+	 * @var integer UID to map a realty object to it
+	 */
+	const UID = 1000;
+
+	public function setUp() {
+		$this->testingFramework = new tx_oelib_testingFramework('tx_realty');
+		$this->testingFramework->createFakeFrontEnd();
+
+		$this->fixture = new tx_realty_pi1_HeadingView(
+			array('templateFile' => 'EXT:realty/pi1/tx_realty_pi1.tpl.htm'),
+			$GLOBALS['TSFE']->cObj,
+			true
+		);
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework);
+	}
+
+
+	/////////////////////////////
+	// Testing the heading view
+	/////////////////////////////
+
+	public function testRenderReturnsEmptyResultForShowUidOfDeletedRecord() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('deleted', 1);
+
+		$this->assertEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsNonEmptyResultForShowUidOfExistingRecord() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', 'test title');
+
+		$this->assertNotEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsNoUnreplacedMarkersWhileTheResultIsNonEmpty() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', 'test title');
+
+		$result = $this->fixture->render(array('showUid' => self::UID));
+
+		$this->assertNotEquals(
+			'',
+			$result
+		);
+		$this->assertNotContains(
+			'###',
+			$result
+		);
+	}
+
+	public function testRenderReturnsTheRealtyObjectsTitleForValidRealtyObject() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', 'test title');
+
+		$this->assertContains(
+			'test title',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsTheRealtyObjectsTitleHtmlspecialcharedForValidRealtyObject() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', 'test</br>title');
+
+		$this->assertContains(
+			htmlspecialchars('test</br>title'),
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsEmptyResultForEmptyTitleOfValidRealtyObject() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', '');
+
+		$this->assertEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+}
+?>
