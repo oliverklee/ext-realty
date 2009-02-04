d2303683d125a495a6e64b9bdefcb8df41cb249e
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-04 17:55:49 +0000

    [Bug 2452] Further split the single view parts into separate views, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@16443 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tests/tx_realty_pi1_AddressView_testcase.php
@@ -0,0 +1,131 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Unit tests for the tx_realty_pi1_AddressView class in the 'realty'
+ * extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_pi1_AddressView_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_realty_pi1_AddressView
+	 */
+	private $fixture;
+
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	/**
+	 * @var integer UID to map a realty object to it
+	 */
+	const UID = 1000;
+
+	public function setUp() {
+		$this->testingFramework = new tx_oelib_testingFramework('tx_realty');
+		$this->testingFramework->createFakeFrontEnd();
+
+		$this->fixture = new tx_realty_pi1_AddressView(
+			array('templateFile' => 'EXT:realty/pi1/tx_realty_pi1.tpl.htm'),
+			$GLOBALS['TSFE']->cObj,
+			true
+		);
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework);
+	}
+
+
+	/////////////////////////////
+	// Testing the heading view
+	/////////////////////////////
+
+	public function testRenderReturnsEmptyResultForShowUidOfDeletedRecord() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('deleted', 1);
+
+		$this->assertEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsNonEmptyResultForShowUidOfExistingRecordWithZip() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('zip', '12345');
+
+		$this->assertNotEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsNoUnreplacedMarkersWhileTheResultIsNonEmpty() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('zip', '12345');
+
+		$result = $this->fixture->render(array('showUid' => self::UID));
+
+		$this->assertNotEquals(
+			'',
+			$result
+		);
+		$this->assertNotContains(
+			'###',
+			$result
+		);
+	}
+
+	public function testRenderReturnsTheRealtyObjectsAddressForValidRealtyObject() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('zip', '12345');
+
+		$this->assertContains(
+			'12345',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+
+	public function testRenderReturnsEmptyResultForEmptyAddressDataOfValidRealtyObject() {
+		tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->find(self::UID)->setProperty('title', '');
+
+		$this->assertEquals(
+			'',
+			$this->fixture->render(array('showUid' => self::UID))
+		);
+	}
+}
+?>
