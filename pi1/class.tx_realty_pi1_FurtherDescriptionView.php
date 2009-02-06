eed100c4ac536d62f5d757cd199cfaa73d44008e
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-06 11:58:06 +0000

    [Bug 2452] Further split the single view parts into separate views, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@16600 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_realty_pi1_FurtherDescriptionView.php
+++ pi1/class.tx_realty_pi1_FurtherDescriptionView.php
@@ -23,40 +23,46 @@
 ***************************************************************/
 
 /**
- * Class 'tx_realty_pi1_DescriptionView' for the 'realty' extension.
+ * Class 'tx_realty_pi1_FurtherDescriptionView' for the 'realty' extension.
  *
- * This class renders the description of a single realty object.
+ * This class renders the description of "equipment" and "location" and the
+ * "misc" text field of a single realty object.
  *
  * @package TYPO3
  * @subpackage tx_realty
  *
  * @author Saskia Metzler <saskia@merlin.owl.de>
  */
-class tx_realty_pi1_DescriptionView extends tx_realty_pi1_FrontEndView {
+class tx_realty_pi1_FurtherDescriptionView extends tx_realty_pi1_FrontEndView {
 	/**
-	 * Returns the description view as HTML.
+	 * Returns the further-description view as HTML.
 	 *
 	 * @param array piVars array, must contain the key "showUid" with a valid
 	 *              realty object UID as value
 	 *
-	 * @return string HTML for the description view, will be empty if the
-	 *                realty object with the provided UID has no description
+	 * @return string HTML for the further-description view will be epmty if
+	 *                the realty object with the provided UID has neither data
+	 *                in "equipment" nor "loaction" nor "misc"
 	 */
 	public function render(array $piVars = array()) {
-		$description = $this->pi_RTEcssText(
-			tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
-				->find($piVars['showUid'])->getProperty('description')
-		);
+		$hasContent = false;
 
-		$this->setOrDeleteMarkerIfNotEmpty(
-			'description', $description, '', 'field_wrapper'
-		);
+		foreach (array('equipment', 'location', 'misc') as $key) {
+			$value = $this->pi_RTEcssText(
+				tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+					->find($piVars['showUid'])->getProperty($key)
+			);
 
-		return $this->getSubpart('FIELD_WRAPPER_DESCRIPTION');
+			$hasContent = $hasContent || $this->setOrDeleteMarkerIfNotEmpty(
+				$key, $value, '', 'field_wrapper'
+			);
+		}
+
+		return ($hasContent ? $this->getSubpart('WRAPPER_FURTHER_DESCRIPTION') : '');
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_DescriptionView.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_DescriptionView.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_FurtherDescriptionView.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_FurtherDescriptionView.php']);
 }
 ?>
