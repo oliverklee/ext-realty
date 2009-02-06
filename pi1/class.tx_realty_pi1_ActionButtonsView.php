1023129281625951aa590351afcfcada7cfd106f
sas-me <sas-me@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-06 10:57:53 +0000

    [Bug 2452] Further split the single view parts into separate views, r=Oliver
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@16569 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/class.tx_realty_pi1_ActionButtonsView.php
@@ -0,0 +1,64 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Class 'tx_realty_pi1_ActionButtonsView' for the 'realty' extension.
+ *
+ * This class renders the add-to-favorites button and the buttons for back and
+ * print.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_pi1_ActionButtonsView extends tx_realty_pi1_FrontEndView {
+	/**
+	 * Returns the add-to-favorites button and the buttons for back and
+	 * print as HTML. For the add-to-favorites button, "favoritesPID" is
+	 * required to be configured.
+	 *
+	 * @param array piVars array, must contain the key "showUid" with a valid
+	 *              realty object UID as value
+	 *
+	 * @return string HTML for the buttons, will not be empty
+	 */
+	public function render(array $piVars = array()) {
+		$favoritesUrl = htmlspecialchars(
+			$this->cObj->typoLink_URL(
+				array('parameter' => $this->getConfValueInteger('favoritesPID'))
+			)
+		);
+
+		$this->setMarker('favorites_url', $favoritesUrl);
+		$this->setMarker('uid', $piVars['showUid']);
+
+		return $this->getSubpart('WRAPPER_ADD_TO_FAVORITES');
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_ActionButtonsView.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/realty/pi1/class.tx_realty_pi1_ActionButtonsView.php']);
+}
+?>
---
+++ tests/tx_realty_pi1_ActionButtonsView_testcase.php
@@ -0,0 +1,124 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 Saskia Metzler <saskia@merlin.owl.de>
+* All rights reserved
+*
+* This script is part of the TYPO3 project. The TYPO3 project is
+* free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('oelib') . 'class.tx_oelib_Autoloader.php');
+
+/**
+ * Unit tests for the tx_realty_pi1_ActionButtonsView class in the 'realty'
+ * extension.
+ *
+ * @package TYPO3
+ * @subpackage tx_realty
+ *
+ * @author Saskia Metzler <saskia@merlin.owl.de>
+ */
+class tx_realty_pi1_ActionButtonsView_testcase extends tx_phpunit_testcase {
+	/**
+	 * @var tx_realty_pi1_ActionButtonsView
+	 */
+	private $fixture;
+
+	/**
+	 * @var tx_oelib_testingFramework
+	 */
+	private $testingFramework;
+
+	public function setUp() {
+		$this->testingFramework = new tx_oelib_testingFramework('tx_realty');
+		$this->testingFramework->createFakeFrontEnd();
+
+		$this->fixture = new tx_realty_pi1_ActionButtonsView(
+			array('templateFile' => 'EXT:realty/pi1/tx_realty_pi1.tpl.htm'),
+			$GLOBALS['TSFE']->cObj
+		);
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+
+		$this->fixture->__destruct();
+		unset($this->fixture, $this->testingFramework);
+	}
+
+
+	////////////////////////////////////
+	// Testing the action buttons view
+	////////////////////////////////////
+
+	public function testRenderReturnsNonEmptyResultForZeroShowUidAndNoFavoritesPidConfigured() {
+		$this->assertNotEquals(
+			'',
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+
+	public function testRenderReturnsButtonBack() {
+		$this->assertContains(
+			'class="button back"',
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+
+	public function testRenderReturnsButtonPrint() {
+		$this->assertContains(
+			'class="button printPage"',
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+
+	public function testRenderReturnsButtonAddToFavorites() {
+		$this->assertContains(
+			'class="button addToFavorites"',
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+
+	public function testRenderReturnsProvidedShowUidOfRealtyRecordWithinTheFormAsInputValue() {
+		$realtyObject = tx_oelib_MapperRegistry::get('tx_realty_Mapper_RealtyObject')
+			->getNewGhost();
+
+		$this->assertContains(
+			'value="' . $realtyObject->getUid() . '"',
+			$this->fixture->render(array('showUid' => $realtyObject->getUid()))
+		);
+	}
+
+	public function testRenderReturnsConfiguredFavoritesPidAsLinkTarget() {
+		$pageUid = $this->testingFramework->createFrontEndPage();
+		$this->fixture->setConfigurationValue('favoritesPID', $pageUid);
+
+		$this->assertContains(
+			'?id=' . $pageUid,
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+
+	public function testRenderReturnsNoUnreplacedMarkersWhileTheResultIsNonEmpty() {
+		$this->assertNotContains(
+			'###',
+			$this->fixture->render(array('showUid' => 0))
+		);
+	}
+}
+?>
