4e044a7294348131c46693c8a9ea46f024a0bb2b
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-21 12:04:29 +0000

    [Bug 1740] Delete the editorcfg entry from ext_localconf.php plus the related files, r=saskia
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@19299 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -92,11 +92,6 @@ t3lib_extMgm::addUserTSConfig('
 	options.saveDocNew.tx_realty_districts = 1
 ');
 
-  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_realty_pi1 = < plugin.tx_realty_pi1.CSS_editor
-',43);
-
 t3lib_extMgm::addPItoST43(
 	$_EXTKEY,
 	'pi1/class.tx_realty_pi1.php',
