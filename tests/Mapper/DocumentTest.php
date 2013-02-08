c5ddb024bd0f9a874c378a4fd562784b9f3eb7dc
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-08 18:13:17 +0000

    [Bug 4630] Update the copyright to 2013
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/trunk@70991 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/Mapper/DocumentTest.php
+++ tests/Mapper/DocumentTest.php
@@ -2,7 +2,7 @@
 /***************************************************************
 * Copyright notice
 *
-* (c) 2010-2012 Bernd Schönbach <bernd.schoenbach@googlemail.com>
+* (c) 2010-2013 Bernd Schönbach <bernd.schoenbach@googlemail.com>
 * All rights reserved
 *
 * This script is part of the TYPO3 project. The TYPO3 project is
