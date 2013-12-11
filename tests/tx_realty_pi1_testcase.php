4c24466f921d5821c4fe6ff59b40b4df86e208ab
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-11 19:35:22 +0000

    [Bug 4363] Skip the configuration check in the unit tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/realty/branches/version_0_5_x@81957 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/tx_realty_pi1_testcase.php
+++ tests/tx_realty_pi1_testcase.php
@@ -123,6 +123,8 @@ class tx_realty_pi1_testcase extends tx_phpunit_testcase {
 	const DE = 54;
 
 	public function setUp() {
+		tx_oelib_configurationProxy::getInstance('realty')->setAsBoolean('enableConfigCheck', FALSE);
+
 		tx_oelib_headerProxyFactory::getInstance()->enableTestMode();
 		$this->testingFramework = new tx_oelib_testingFramework('tx_realty');
 		$this->testingFramework->createFakeFrontEnd();
