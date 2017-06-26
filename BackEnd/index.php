eedf6839485939e064c39b5b3759b85c3069054c
Oliver Klee <typo3-coding@oliverklee.de>
2017-06-26 19:20:00 +0200

    [TASK] Link the crowdfunding campaign
    Change-Id: I86f2d116024dc267a46d62c27bf2c537bc2392f7
    Reviewed-on: https://review.typo3.org/53332
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -21,3 +21,16 @@ $SOBE = t3lib_div::makeInstance('tx_realty_BackEnd_Module');
 $SOBE->init();
 
 echo $SOBE->render();
+
+echo <<<EOT
+
+<h2>Compatibility with TYPO3 CMS 7.6 and 8.7</h2>
+
+<p>
+
+Currently, there is a <a href="https://docs.google.com/spreadsheets/d/1BfSmradrQMrcbnABqCpo0gGbgNpfNtGzy94Lpvd8Grk/pubhtml" target="_blank" style="text-decoration: underline;">
+crowdfunding campaign</a> going on to fund the development work needed to make this extension compatible with TYPO3 CMS 7.6 and 8.7. If you would
+like to see this extension work on TYPO3 CMS 7.6 and 8.7, please consider contributing to the campaign and
+send the extension author an email: <a href="mailto:typo3-coding@oliverklee.de" style="text-decoration: underline;">typo3-coding@oliverklee.de</a>
+</p>
+EOT;
