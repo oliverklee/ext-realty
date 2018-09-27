4046ffad05e3f6d85f5c810a4dfed8793f6643ee
Oliver Klee <github@oliverklee.de>
2018-09-27 17:29:05 +0200

    [CLEANUP] Autoformat all non-PHP text files (#58)

--- pi1/tx_realty_pi1.js
+++ pi1/tx_realty_pi1.js
@@ -28,17 +28,17 @@
  * @param {string} confirmMessage localized confirm message for whether really to mark an attachment for deletion
  */
 function markAttachmentAsDeleted(listItemId, confirmMessage) {
-	var listItem = document.getElementById(listItemId);
-	var fileNameDiv = listItem.getElementsByTagName("span")[0];
-	var deleteButton = listItem.getElementsByTagName("input")[0];
+    var listItem = document.getElementById(listItemId);
+    var fileNameDiv = listItem.getElementsByTagName("span")[0];
+    var deleteButton = listItem.getElementsByTagName("input")[0];
 
-	if (confirm(confirmMessage)) {
-		document.getElementById("tx_realty_frontEndImageUpload_imagesToDelete").value
-			+= "," + listItemId;
-		fileNameDiv.setAttribute("class", "deleted");
-		deleteButton.className += " deleted";
-		deleteButton.disabled = true;
-	}
+    if (confirm(confirmMessage)) {
+        document.getElementById("tx_realty_frontEndImageUpload_imagesToDelete").value
+            += "," + listItemId;
+        fileNameDiv.setAttribute("class", "deleted");
+        deleteButton.className += " deleted";
+        deleteButton.disabled = true;
+    }
 }
 
 /**
@@ -46,25 +46,25 @@ function markAttachmentAsDeleted(listItemId, confirmMessage) {
  * "rent" or "sale" is selected for the edited object.
  */
 function updateHideAndShow() {
-	if ($("tx_realty_frontEndEditor_object_type_item").checked) {
-		$$(".rent").invoke("show");
-		$$(".sale").invoke("hide");
-	} else {
-		$$(".rent").invoke("hide");
-		$$(".sale").invoke("show");
-	}
+    if ($("tx_realty_frontEndEditor_object_type_item").checked) {
+        $$(".rent").invoke("show");
+        $$(".sale").invoke("hide");
+    } else {
+        $$(".rent").invoke("hide");
+        $$(".sale").invoke("show");
+    }
 
-	if ($("tx_realty_frontEndEditor_contact_data_source_item").checked) {
-		$("contact-data").show();
-	} else {
-		$("contact-data").hide();
-	}
+    if ($("tx_realty_frontEndEditor_contact_data_source_item").checked) {
+        $("contact-data").show();
+    } else {
+        $("contact-data").hide();
+    }
 
-	if ($("tx_realty_frontEndEditor_has_coordinates").checked) {
-		$$(".coordinates").invoke("show");
-	} else {
-		$$(".coordinates").invoke("hide");
-	}
+    if ($("tx_realty_frontEndEditor_has_coordinates").checked) {
+        $$(".coordinates").invoke("show");
+    } else {
+        $$(".coordinates").invoke("hide");
+    }
 }
 
 /**
@@ -75,32 +75,32 @@ function updateHideAndShow() {
  * This function must only be called if Prototype is loaded.
  */
 function updateDistrictsInSearchWidget() {
-	if (!$("tx_realty_pi1-city") || !$("tx_realty_pi1-district")) {
-		return;
-	}
+    if (!$("tx_realty_pi1-city") || !$("tx_realty_pi1-district")) {
+        return;
+    }
 
-	var cityUid = $("tx_realty_pi1-city").value;
-	if (cityUid == "0") {
-		Element.hide($("tx_realty_pi1_searchWidget_district"));
-		return;
-	}
+    var cityUid = $("tx_realty_pi1-city").value;
+    if (cityUid == "0") {
+        Element.hide($("tx_realty_pi1_searchWidget_district"));
+        return;
+    }
 
-	new Ajax.Updater(
-		"tx_realty_pi1-district",
-		"/index.php?eID=realty&type=withNumber&city=" + encodeURI(cityUid),
-		{
-			method: "get",
-			onLoading: function() {
-				$("tx_realty_pi1-district").disabled = true;
-				Element.show($("tx_realty_pi1_searchWidget_district"));
-				Element.show($("tx-realty-pi1-loading"));
-			},
-			onComplete: function() {
-				$("tx_realty_pi1-district").disabled = false;
-				Element.hide($("tx-realty-pi1-loading"));
-			}
-		}
-	);
+    new Ajax.Updater(
+        "tx_realty_pi1-district",
+        "/index.php?eID=realty&type=withNumber&city=" + encodeURI(cityUid),
+        {
+            method: "get",
+            onLoading: function () {
+                $("tx_realty_pi1-district").disabled = true;
+                Element.show($("tx_realty_pi1_searchWidget_district"));
+                Element.show($("tx-realty-pi1-loading"));
+            },
+            onComplete: function () {
+                $("tx_realty_pi1-district").disabled = false;
+                Element.hide($("tx-realty-pi1-loading"));
+            }
+        }
+    );
 }
 
 /**
@@ -110,36 +110,36 @@ function updateDistrictsInSearchWidget() {
  * If no city is selected, the district element will be hidden.
  */
 function updateDistrictsInEditor() {
-	if (!$("tx_realty_frontEndEditor_city")
-		|| !$("tx_realty_frontEndEditor_district_wrapper")
-		|| !$("tx_realty_frontEndEditor_district")
-		|| !$("tx_realty_frontEndEditor_new_district_wrapper")
-	) {
-		return;
-	}
+    if (!$("tx_realty_frontEndEditor_city")
+        || !$("tx_realty_frontEndEditor_district_wrapper")
+        || !$("tx_realty_frontEndEditor_district")
+        || !$("tx_realty_frontEndEditor_new_district_wrapper")
+    ) {
+        return;
+    }
 
-	var cityUid = $("tx_realty_frontEndEditor_city").value;
-	if (cityUid == "0") {
-		Element.hide($("tx_realty_frontEndEditor_district_wrapper"));
-		Element.hide($("tx_realty_frontEndEditor_new_district_wrapper"));
-		return;
-	}
+    var cityUid = $("tx_realty_frontEndEditor_city").value;
+    if (cityUid == "0") {
+        Element.hide($("tx_realty_frontEndEditor_district_wrapper"));
+        Element.hide($("tx_realty_frontEndEditor_new_district_wrapper"));
+        return;
+    }
 
-	new Ajax.Updater(
-		"tx_realty_frontEndEditor_district",
-		"/index.php?eID=realty&city=" + encodeURI(cityUid),
-		{
-			method: "get",
-			onLoading: function() {
-				$("tx_realty_frontEndEditor_district").disabled = true;
-				Element.show($("tx_realty_frontEndEditor_district_wrapper"));
-				Element.show($("tx_realty_frontEndEditor_new_district_wrapper"));
-			},
-			onComplete: function() {
-				$("tx_realty_frontEndEditor_district").disabled = false;
-			}
-		}
-	);
+    new Ajax.Updater(
+        "tx_realty_frontEndEditor_district",
+        "/index.php?eID=realty&city=" + encodeURI(cityUid),
+        {
+            method: "get",
+            onLoading: function () {
+                $("tx_realty_frontEndEditor_district").disabled = true;
+                Element.show($("tx_realty_frontEndEditor_district_wrapper"));
+                Element.show($("tx_realty_frontEndEditor_new_district_wrapper"));
+            },
+            onComplete: function () {
+                $("tx_realty_frontEndEditor_district").disabled = false;
+            }
+        }
+    );
 }
 
 /**
@@ -149,12 +149,12 @@ function updateDistrictsInEditor() {
  * @param {string} title the title of the district, must not be empty
  */
 function appendDistrictInEditor(uid, title) {
-	var container = $("tx_realty_frontEndEditor_district");
-	if (!container) {
-		return;
-	}
-	var optionElement = new Element("option", {"value": uid});
-	optionElement.appendChild(document.createTextNode(title));
+    var container = $("tx_realty_frontEndEditor_district");
+    if (!container) {
+        return;
+    }
+    var optionElement = new Element("option", {"value": uid});
+    optionElement.appendChild(document.createTextNode(title));
 
-	container.appendChild(optionElement);
+    container.appendChild(optionElement);
 }
