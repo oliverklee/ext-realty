303f5c8e3136cbe2dd076bc9456c19a96e4cee96
Oliver Klee <typo3-coding@oliverklee.de>
2018-10-02 15:38:40 +0200

    [TASK] Remove the crowdfunding campaign from the docs
    [ci skip]

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -9,16 +9,3 @@ $SOBE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_realty_BackEnd_
 $SOBE->init();
 
 echo $SOBE->render();
-
-echo <<<EOT
-
-<h2>Compatibility with TYPO3 CMS 7.6 and 8.7</h2>
-
-<p>
-
-Currently, there is a <a href="https://docs.google.com/spreadsheets/d/1BfSmradrQMrcbnABqCpo0gGbgNpfNtGzy94Lpvd8Grk/pubhtml" target="_blank" style="text-decoration: underline;">
-crowdfunding campaign</a> going on to fund the development work needed to make this extension compatible with TYPO3 CMS 7.6 and 8.7. If you would
-like to see this extension work on TYPO3 CMS 7.6 and 8.7, please consider contributing to the campaign and
-send the extension author an email: <a href="mailto:typo3-coding@oliverklee.de" style="text-decoration: underline;">typo3-coding@oliverklee.de</a>
-</p>
-EOT;
