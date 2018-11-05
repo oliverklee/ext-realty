0c4d53cfd0f7bf9e93d62e355b90caf28b500fce
Oliver Klee <typo3-coding@oliverklee.de>
2018-11-05 19:19:47 +0100

    [CLEANUP] Clean up localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,92 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-
-	# ***************************************************************************************
-	# CONFIGURATION of RTE in table "tx_realty_objects", field "description"
-	# ***************************************************************************************
-RTE.config.tx_realty_objects.description {
-  hidePStyleItems = H1, H4, H5, H6
-  proc.exitHTMLparser_db = 1
-  proc.exitHTMLparser_db {
-    keepNonMatchedTags = 1
-    tags.font.allowedAttribs = color
-    tags.font.rmTagIfNoAttrib = 1
-    tags.font.nesting = global
-  }
-}
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-
-	# ***************************************************************************************
-	# CONFIGURATION of RTE in table "tx_realty_objects", field "equipment"
-	# ***************************************************************************************
-RTE.config.tx_realty_objects.equipment {
-  hidePStyleItems = H1, H4, H5, H6
-  proc.exitHTMLparser_db = 1
-  proc.exitHTMLparser_db {
-    keepNonMatchedTags = 1
-    tags.font.allowedAttribs = color
-    tags.font.rmTagIfNoAttrib = 1
-    tags.font.nesting = global
-  }
-}
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-
-	# ***************************************************************************************
-	# CONFIGURATION of RTE in table "tx_realty_objects", field "location"
-	# ***************************************************************************************
-RTE.config.tx_realty_objects.location {
-  hidePStyleItems = H1, H4, H5, H6
-  proc.exitHTMLparser_db = 1
-  proc.exitHTMLparser_db {
-    keepNonMatchedTags = 1
-    tags.font.allowedAttribs = color
-    tags.font.rmTagIfNoAttrib = 1
-    tags.font.nesting = global
-  }
-}
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-
-	# ***************************************************************************************
-	# CONFIGURATION of RTE in table "tx_realty_objects", field "misc"
-	# ***************************************************************************************
-RTE.config.tx_realty_objects.misc {
-  hidePStyleItems = H1, H4, H5, H6
-  proc.exitHTMLparser_db = 1
-  proc.exitHTMLparser_db {
-    keepNonMatchedTags = 1
-    tags.font.allowedAttribs= color
-    tags.font.rmTagIfNoAttrib = 1
-    tags.font.nesting = global
-  }
-}
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_objects = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_apartment_types = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_house_types = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_car_places = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_pets = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_cities = 1
-');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-	options.saveDocNew.tx_realty_districts = 1
-');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
     $_EXTKEY,
     'pi1/class.tx_realty_pi1.php',
@@ -98,11 +12,9 @@ RTE.config.tx_realty_objects.misc {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
     $_EXTKEY,
     'setup',
-    '
-	tt_content.shortcut.20.conf.tx_realty_objects = < plugin.'
-    . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN($_EXTKEY) . '_pi1
-	tt_content.shortcut.20.conf.tx_realty_objects.CMD = singleView
-',
+    "\ntt_content.shortcut.20.conf.tx_realty_objects = < plugin."
+    . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN($_EXTKEY) . "_pi1\n" .
+    "tt_content.shortcut.20.conf.tx_realty_objects.CMD = singleView\n",
     43
 );
 
