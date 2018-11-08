e72739e73a73dd9be07b5593ace7c52ae0717ea4
Oliver Klee <typo3-coding@oliverklee.de>
2018-11-08 16:39:27 +0100

    [CLEANUP] Use the literal "realty" instead of `$_EXTKEY`

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
-    $_EXTKEY,
+    'realty',
     'pi1/class.tx_realty_pi1.php',
     '_pi1',
     'list_type',
@@ -10,16 +10,16 @@ defined('TYPO3_MODE') or die('Access denied.');
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    $_EXTKEY,
+    'realty',
     'setup',
     "\ntt_content.shortcut.20.conf.tx_realty_objects = < plugin."
-    . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN($_EXTKEY) . "_pi1\n" .
+    . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getCN('realty') . "_pi1\n" .
     "tt_content.shortcut.20.conf.tx_realty_objects.CMD = singleView\n",
     43
 );
 
 // registers the eID functions for AJAX
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Ajax/tx_realty_Ajax_Dispatcher.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['realty'] = 'EXT:realty/Ajax/tx_realty_Ajax_Dispatcher.php';
 
 // RealURL autoconfiguration
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['realty']
