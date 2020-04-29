fc1de8e39d8fd07c7a74463a394fcf4aab54c7d7
Oliver Klee <typo3-coding@oliverklee.de>
2020-04-29 18:44:28 +0200

    [BUGFIX] Catch exceptions in the BE module (#293)

--- Classes/Controller/OpenImmoController.php
+++ Classes/Controller/OpenImmoController.php
@@ -44,7 +44,19 @@ class OpenImmoController extends ActionController
      */
     public function importAction()
     {
-        $this->view->assign('importResults', $this->importService->importFromZip());
-        $this->view->assign('importStatus', $this->importService->wasSuccessful() ? 0 : 2);
+        $importResults = '';
+        try {
+            $importResults = $this->importService->importFromZip();
+            $success = $this->importService->wasSuccessful();
+        } catch (\Exception $exception) {
+            $backTrace = \json_encode(
+                $exception->getTrace(),
+                JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+            );
+            $importResults .= $exception->getMessage() . "\n" . $backTrace;
+            $success = false;
+        }
+        $this->view->assign('importResults', $importResults);
+        $this->view->assign('importStatus', $success ? 0 : 2);
     }
 }
