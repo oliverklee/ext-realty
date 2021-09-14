abe750d6286edbcad1ad15c5d23b8e492b596e53
Fawad Mirzad <fawmi@users.noreply.github.com>
2021-09-14 15:57:33 +0200

    [Bugfix]: Fix the list plugin showing error in TYPO3 9

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,33 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['realty_pi1'] = 'layout,select_key,pages,recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['realty_pi1'] = 'pi_flexform';
+$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('realty');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'realty',
+    'Configuration/TypoScript/',
+    'Realty Manager'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'OliverKlee.Realty',
+    'web',
+    'openImmo',
+    'bottom',
+    [
+        'OpenImmo' => 'index, import',
+    ],
+    [
+        'access' => 'group',
+        'icon' => 'EXT:realty/Resources/Public/Icons/BackEndModule.gif',
+        'labels' => 'LLL:EXT:realty/Resources/Private/Language/locallang_mod.xlf',
+        // hide the page tree
+        'navigationComponentId' => '',
+    ]
+);
+
+// Include base TSconfig setup
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:realty/Configuration/TSconfig/ContentElementWizard.tsconfig'"
+);
+
